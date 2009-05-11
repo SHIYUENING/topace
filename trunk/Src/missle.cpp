dd8c6df8a548e6837cfb72738703c84502e84dba
r_ex@163.com <r_ex@163.com@f77f3694-0ae2-11de-a6fd-4d0cda92487f>
2009-05-11 11:22:02 +0000

    修正使用点光源模拟太阳光处的一个问题

--- trunk/Src/missle.cpp
+++ trunk/Src/missle.cpp
@@ -19,23 +19,20 @@ Missledata::~Missledata(void)
 void Missledata::TurnTo(const Vector3d& Position){
 	Vector3d current;
     current = UDMplane.Matrix() * Vector3d(0, 0, 1);
-    if (!all_elements(current == 0)){
-        current = normalize(current);
-    }
+    if (all_elements(current == 0)){ return; }
+    current = normalize(current);
 
     Vector3d target;
     target = Position - UDMplane.RefPos();
-    if (!all_elements(target == 0)){
-        target = normalize(target);
-    }
+    if (all_elements(target == 0)){ return; }
+    target = normalize(target);
 
     double k = dot(current, target);
 
     Vector3d rotateAxis;
     rotateAxis = cross(current, target);
-    if (!all_elements(rotateAxis == 0)){
-        rotateAxis = normalize(rotateAxis);
-    }
+    if (all_elements(rotateAxis == 0)){ return; }
+    rotateAxis = normalize(rotateAxis);
 
     double rotateAngle = acos_s(dot(current, target));
 
