9b6146641b26f12b8996e134bbfa8a0e24c2b149
ezoz01 <ezoz01@gmail.com>
2009-05-19 07:12:35 +0000

    无关紧要的小修改

--- Src/initdata.h
+++ Src/initdata.h
@@ -74,11 +74,20 @@ void initUnitdata(int stage=0 )
 			UDfighers[i].UDfighterType=2;
 			UDfighers[i].UDlife=99;
 			UDfighers[i].UDflag=(i/5)%4;
-			sprintf(UDfighers[i].UDname,"Neo1");
+			if(UDfighers[i].UDflag==2)
+				sprintf(UDfighers[i].UDname,"Mave");
+			else
+				sprintf(UDfighers[i].UDname,"Neo1");
 			UDfighers[i].UDlockselect=false;
 			UDfighers[i].UDMplane.Translate(Vector3d(500.0f*(i-maxUnits/2), 35000.0f+5000.0f*(i%4), 2000.0f*(i%20-10)));
 			UDfighers[i].MoveToPos=Vector3d(500.0f*(i-maxUnits/2), 35000.0f+5000.0f*(i%4), 2000.0f*(i%20-10));
 			UDfighers[i].UDMplane.RotateInternal(Vector3d(0.0f, 1.0f, 0.0f) * (i%8+2));
+			if(UDfighers[i].UDflag==2)
+			{
+				UDfighers[i].UDfighterType=3;
+				UDfighers[i].LockOnTime=1;
+				UDfighers[i].RefireTime=450;
+			}
 		}
 
 	}
