0417b1cd05d6e4830e17f17b1e414f9fe95648c5
ezoz01@gmail.com <ezoz01@gmail.com@f77f3694-0ae2-11de-a6fd-4d0cda92487f>
2009-05-21 08:13:40 +0000

    字库处理基本完成

--- trunk/Src/MyFont.cpp
+++ trunk/Src/MyFont.cpp
@@ -1,14 +1,28 @@
 #include "MyFont.h"
 CMyFont::CMyFont(void)
 :WordNum(0)
+,WordList(0)
+,ifloadedFont(false)
 {
 	OneFontpixels = new unsigned char[0x1000];
+	WordList=glGenLists(1);
+	glNewList(WordList,GL_COMPILE);
+		glBegin(GL_QUADS);
+			glTexCoord2f(0.0f,1.0f);glVertex2f(0.0f,1.0f);
+			glTexCoord2f(0.0f,0.0f);glVertex2f(0.0f,0.0f);
+			glTexCoord2f(1.0f,0.0f);glVertex2f(1.0f,0.0f);
+			glTexCoord2f(1.0f,1.0f);glVertex2f(1.0f,1.0f);
+		glEnd();
+	glEndList();
 		
 }
 
 CMyFont::~CMyFont(void)
 {
 	delete [] OneFontpixels;
+	delete [] MyFontpixels;
+	ClearTXT();
+	glDeleteLists(WordList,1);
 }
 
 bool CMyFont::LoadFont(const char *filename)
@@ -33,13 +47,14 @@ bool CMyFont::LoadFont(const char *filename)
 		MyFontpixels = new unsigned char[filesize];
 		fread( MyFontpixels, 1, filesize, m_FilePointer );
 		fclose(m_FilePointer);
+		ifloadedFont=true;
 		return true;
 	}
 
 	return false;
 }
 
-int CMyFont::GetFontIndex(int InH,int InL)
+int CMyFont::GetFontIndex(BYTE InH,BYTE InL)
 {
 
 	return (InH-0xA1)*(0xFE-0xA1+1)+InL-0xA1;
@@ -51,6 +66,7 @@ unsigned int CMyFont::CreatFont(int FontIndex)
 	{
 		BYTE pixels8 = MyFontpixels[FontIndex*0x80+i];
 		bool bpixels[8];
+		/*
 		bpixels[0] = bool(pixels8 & 1);
 		bpixels[1] = bool(pixels8 & 2);
 		bpixels[2] = bool(pixels8 & 4);
@@ -59,6 +75,16 @@ unsigned int CMyFont::CreatFont(int FontIndex)
 		bpixels[5] = bool(pixels8 & 0x20);
 		bpixels[6] = bool(pixels8 & 0x40);
 		bpixels[7] = bool(pixels8 & 0x80);
+		*/
+
+		bpixels[7] = bool(pixels8 & 1);
+		bpixels[6] = bool(pixels8 & 2);
+		bpixels[5] = bool(pixels8 & 4);
+		bpixels[4] = bool(pixels8 & 8);
+		bpixels[3] = bool(pixels8 & 0x10);
+		bpixels[2] = bool(pixels8 & 0x20);
+		bpixels[1] = bool(pixels8 & 0x40);
+		bpixels[0] = bool(pixels8 & 0x80);
 		for(int j=0;j<8;j++)
 		{
 
@@ -68,17 +94,10 @@ unsigned int CMyFont::CreatFont(int FontIndex)
 			if(bpixels[j])
 				OneFontpixels[i*0x20+j*4+3]=0xFF;
 			else
-				OneFontpixels[i*0x20+j*4+3]=0;
-
-		
+				OneFontpixels[i*0x20+j*4+3]=0x0;
 		}
-
-	
 	}
-
 	unsigned int FontTexID;
-
-
 	glGenTextures(1, &FontTexID);				// 创建一个纹理
 	glBindTexture(GL_TEXTURE_2D, FontTexID);			// 构造纹理
 	gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGBA, 32, 32, GL_RGBA, GL_UNSIGNED_BYTE, OneFontpixels);
@@ -90,19 +109,75 @@ unsigned int CMyFont::CreatFont(int FontIndex)
 
 void CMyFont::inputTxt(const char * Chars)
 {
+	if(!ifloadedFont)
+		return;
+	ClearTXT();
 	WordNum=strlen(Chars)/2;
-	DrawTXT=new unsigned int[WordNum*sizeof(unsigned int)];
-	for(int i=0;i<WordNum;i++)
+	if(WordNum<1)
+		return;
+	TXTTexIDs=new unsigned int[WordNum*sizeof(unsigned int)];
+	for(unsigned int i=0;i<WordNum;i++)
 	{
 		//if(Chars[i*2]*Chars[i*2+1]==0)
 			//return;
-		DrawTXT[i]=CreatFont(GetFontIndex(Chars[i*2],Chars[i*2+1]));
-	
+		TXTTexIDs[i]=CreatFont(GetFontIndex(Chars[i*2],Chars[i*2+1]));
 	}
 }
 
 void CMyFont::ClearTXT(void)
 {
-	if(WordNum==0)
+	if(WordNum<1)
 		return;
+	for(unsigned int i=0;i<WordNum;i++)
+	{
+		if(TXTTexIDs[i]!=0)
+		glDeleteTextures(1,&TXTTexIDs[i]);
+	}
+	delete [] TXTTexIDs;
+}
+
+void CMyFont::DrawTXT(int WinW, int WinH, int PosX, int PosY, int SizeW, int SizeH)
+{
+	if(WordNum<1)
+		return;
+	int WinPosX=0;
+	int WinPosY=WinH;
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA   );
+				// Select Our Font Texture
+	glDisable(GL_DEPTH_TEST);							// Disables Depth Testing
+	glMatrixMode(GL_PROJECTION);						// Select The Projection Matrix
+	glPushMatrix();										// Store The Projection Matrix
+	glLoadIdentity();									// Reset The Projection Matrix
+	glOrtho(0,WinW,0,WinH,-1,1);							// Set Up An Ortho Screen
+	glMatrixMode(GL_MODELVIEW);							// Select The Modelview Matrix
+	glPushMatrix();										// Store The Modelview Matrix
+	for(unsigned int i=0;i<WordNum;i++)
+	{
+		glBindTexture(GL_TEXTURE_2D, TXTTexIDs[i]);
+		glLoadIdentity();
+		WinPosX=WinPosX+SizeW;
+		if(WinPosX>(WinW-PosX))
+		{
+			WinPosX=0;
+			WinPosY=WinPosY-SizeH;
+		}
+		glTranslated(WinPosX+PosX,WinPosY-PosY,0);
+		//glScaled(SizeW,SizeH,0.0);
+		//glCallList(WordList);
+			glBegin(GL_QUADS);
+				glTexCoord2f(0.0f,1.0f);glVertex2i(-SizeW/2,-SizeH/2);
+				glTexCoord2f(1.0f,1.0f);glVertex2i( SizeW/2,-SizeH/2);
+				glTexCoord2f(1.0f,0.0f);glVertex2i( SizeW/2, SizeH/2);
+				glTexCoord2f(0.0f,0.0f);glVertex2i(-SizeW/2, SizeH/2);
+			glEnd();
+	
+	}
+
+
+	glMatrixMode(GL_PROJECTION);						// Select The Projection Matrix
+	glPopMatrix();										// Restore The Old Projection Matrix
+	glMatrixMode(GL_MODELVIEW);							// Select The Modelview Matrix
+	glPopMatrix();										// Restore The Old Projection Matrix
+	glEnable(GL_DEPTH_TEST);							// Enables Depth Testing
 }
