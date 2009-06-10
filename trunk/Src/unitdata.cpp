6011e9db078279bdcefbaec1efc126b3f8283bb5
ezoz01@gmail.com <ezoz01@gmail.com@f77f3694-0ae2-11de-a6fd-4d0cda92487f>
2009-06-10 08:44:27 +0000

    改变了操作手感

--- trunk/Src/unitdata.cpp
+++ trunk/Src/unitdata.cpp
@@ -154,6 +154,33 @@ void Unitdata::setpos(int smokeN)
 
 }
 */
+void Unitdata::TurnTom(const Vector3d& Position){
+	Vector3d current;
+    current = UDMplane.Matrix() * Vector3d(0, 0, 1);
+    if (!all_elements(current == 0)){
+        current = normalize(current);
+    }
+
+    Vector3d target;
+    target = Position - UDMplane.RefPos();
+    if (!all_elements(target == 0)){
+        target = normalize(target);
+    }
+
+    double k = dot(current, target);
+
+    Vector3d rotateAxis;
+    rotateAxis = cross(current, target);
+    if (!all_elements(rotateAxis == 0)){
+        rotateAxis = normalize(rotateAxis);
+    }
+
+    double rotateAngle = acos_s(dot(current, target));
+
+    UDPstate.AngleAcceleration = rotateAxis * rotateAngle - UDPstate.AngleVelocity;
+	//UDPstate.Acceleration = UDMplane.Matrix() * Vector3d(0, 0, 1) * 35;
+	
+}
 void Unitdata::TurnTo(const Vector3d& Position){
 
 	
